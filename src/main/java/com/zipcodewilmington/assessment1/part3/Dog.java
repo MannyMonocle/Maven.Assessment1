f8ae942db101d0e12cc9151bdcbd593cae071347
MannyMonocle <eetumadu@gmail.com>
2019-10-28 16:15:01 -0400

    due diligence commit

--- src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
@@ -8,21 +8,27 @@ public class Dog extends Pet {
      * @param name name of this Dog
      * @param age age of this dog
      */
-    public Dog(String name, Integer age) {
 
+    private String name;
+    private Integer age;
+
+    public Dog(String name, Integer age) {
+        this.name = name;
+        this.age = age;
     }
 
     /**
      * @param age age of this dog
      */
     public Dog(Integer age) {
+        this.age = age;
     }
 
     /**
      * @param name name of this dog
      */
     public Dog(String name) {
-
+        this.name = name;
     }
 
     /**
@@ -32,12 +38,14 @@ public class Dog extends Pet {
      * age is 0
      */
     public Dog() {
+        this.name = "DogName";
+        this.age = 0;
     }
 
     /**
      * @return bark as a string
      */
     public String speak() {
-        return null;
+        return "bark";
     }
 }
